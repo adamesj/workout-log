e7383c3e27721f7b82e93a3e4d17e116e4d153c1
Jimmy Adames <jimmy.adames@gmail.com>
2017-06-28 15:33:06 -0400

    Implemented feature test for editing exercises

--- spec/factories/exercises.rb
+++ spec/factories/exercises.rb
@@ -4,13 +4,12 @@ FactoryGirl.define do
     duration     10
     workout_details  "pushups"
     activity_date Date.today.strftime('%Y-%m-%d')
-    user
+    association :user, factory: :user
 
     trait :invalid_exercise do
       exercise 0
       workout_details "up"
       activity_date (Date.today - 2.days).strftime('%Y-%m-%d')
-      user
     end
   end
 end
