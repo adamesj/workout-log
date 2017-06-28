7722717d6d8a680ae1910a71942f148d62fb4b39
Jimmy Adames <jimmy.adames@gmail.com>
2017-06-28 19:04:26 -0400

    Adjusted registrations controller to accept custom attributes for users

--- spec/features/authentication/user_sign_up_spec.rb
+++ spec/features/authentication/user_sign_up_spec.rb
@@ -3,19 +3,25 @@ require 'rails_helper'
 RSpec.feature "New user session" do
   scenario "with valid credentials" do
     visit '/users/sign_up'
-    fill_in "user[email]", with: "billydkid@mail.com"
-    fill_in "user[password]", with: "secret123"
-    fill_in "user[password_confirmation]", with: "secret123"
+    fill_in "user[first_name]", with: "Donkey"
+    fill_in "user[last_name]", with: "Kong"
+    fill_in "user[email]", with: "dk@mail.com"
+    fill_in "user[password]", with: "banana123"
+    fill_in "user[password_confirmation]", with: "banana123"
     click_button "Sign up"
     expect(page).to have_content("Welcome! You have signed up successfully.")
   end
 
   scenario "with no credentials" do
     visit '/users/sign_up'
+    fill_in "user[first_name]", with: ""
+    fill_in "user[last_name]", with: ""
     fill_in "user[email]", with: ""
     fill_in "user[password]", with: ""
     fill_in "user[password_confirmation]", with: ""
     click_button "Sign up"
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
     expect(page).to have_content("Email can't be blank")
     expect(page).to have_content("Password can't be blank")
   end
