5e3ca4608d330dc98d067f5096504ebaadde28d7
Jimmy Adames <jimmy.adames@gmail.com>
2017-07-01 11:20:53 -0400

    Refactored exercise.js file in order to properly display the data of the user's workouts over the past seven days in the generated d3 graph

--- app/assets/javascripts/exercise.js
+++ app/assets/javascripts/exercise.js
@@ -8,21 +8,16 @@ $(document).on("turbolinks:load", function(){
       $('.navbar-brand').css("color", "#000");
       $('.navbar-default .navbar-nav > li > a').css("color", "#000");
       $('.navbar-default .navbar-brand').css("border", "1px solid #000");
+      // d3 chart
+      drawChart();
     }
-
   $(".datepicker").datepicker({format: 'yyyy/mm/dd'});
-
-  // d3 chart
-  var regex = /\/users\/\d+\/exercises$|\/friendships\/d+$/i;
-  if($(location).attr('pathname').match(regex)) {
-    drawChart();
-  }
 });
 
 var drawChart = function() {
 var margin = { top: 100, right: 20, bottom: 100, left: 50 },
-    width  = 600 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
+  width  = 600 - margin.left - margin.right,
+  height = 450 - margin.top - margin.bottom;
 var JSONData = $("#chart").data("workouts");
 
 if (!JSONData) {
@@ -33,24 +28,24 @@ var data = JSONData.slice()
 
 var parseTime = d3.timeParse("%Y-%m-%d");
 
-var workoutFn = function(d) { return d.duration_in_min }
-var dateFn = function(d) { return parseTime(d.workout_date) }
+var workoutFn = function(d) { return d.duration }
+var dateFn = function(d) { return parseTime(d.activity_date) }
 
 var x = d3.scaleTime()
   .range([0, width])
-  .domain(d3.extent(data, dateFn))
+  .domain([Date.now() - ( 7 * 24 * 60 * 60 * 1000), Date.now()])
 
 var y = d3.scaleLinear()
   .range([height, 0])
   .domain([0, d3.max(data, workoutFn)])
 
 var workout_line = d3.line()
-    .x(function(d) { return x(d.workout_date); })
-    .y(function(d) { return y(d.duration_in_min);  });
+  .x(function(d) { return x(d.activity_date); })
+  .y(function(d) { return y(d.duration);  });
 
 data.forEach(function(d) {
-  d.workout_date = parseTime(d.workout_date);
-  d.duration_in_min = +d.duration_in_min;
+  d.activity_date = parseTime(d.activity_date);
+  d.duration = +d.duration;
 });
 
 var svg = d3.select("#chart").append("svg")
@@ -60,8 +55,8 @@ var svg = d3.select("#chart").append("svg")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   svg.append("path")
-      .attr("class", "line")
-      .attr("d", workout_line(data));
+    .attr("class", "line")
+    .attr("d", workout_line(data));
 
 svg.append("g")
  .attr("class", "x axis")
@@ -70,18 +65,18 @@ svg.append("g")
     .ticks(d3.timeDay.every(1))
     .tickFormat(d3.timeFormat('%Y-%m-%d'))
   )
- .selectAll("text")
-    .style("text-anchor", "end")
-    .attr("dx", "-.8em")
-    .attr("dy", ".15em")
-    .attr("transform", "rotate(-60)");
+  .selectAll("text")
+  .style("text-anchor", "end")
+  .attr("dx", "-.8em")
+  .attr("dy", ".15em")
+  .attr("transform", "rotate(-60)");
 
  // x axis label
  svg.append("text")
-    .attr("x", width / 2)
-    .attr("y", height + margin.top - 15)
-    .style("text-anchor", "middle")
-    .text("Date of workout")
+  .attr("x", width / 2)
+  .attr("y", height + margin.top - 15)
+  .style("text-anchor", "middle")
+  .text("Date of workout")
 
  svg.append("g")
   .attr("class", "y axis")
@@ -89,20 +84,20 @@ svg.append("g")
 
 // y axis label
 svg.append("text")
-   .attr("transform", "rotate(-90)")
-   .attr("y", 0 - margin.left)
-   .attr("x", 0 - (height / 2))
-   .attr("dy", "1em")
-   .style("text-anchor", "middle")
-   .text("Workout duration (min)")
+ .attr("transform", "rotate(-90)")
+ .attr("y", 0 - margin.left)
+ .attr("x", 0 - (height / 2))
+ .attr("dy", "1em")
+ .style("text-anchor", "middle")
+ .text("Workout duration (min)")
 
  // Chart title
  svg.append("text")
-    .attr("x", (width / 2))
-    .attr("y", 0 - (margin.top / 2))
-    .style("text-anchor", "middle")
-    .style("font-size", "18px")
-    .style("text-decoration", "underline")
-    .text("Workout duration vs Workout date")
+  .attr("x", (width / 2))
+  .attr("y", 0 - (margin.top / 2))
+  .style("text-anchor", "middle")
+  .style("font-size", "18px")
+  .style("text-decoration", "underline")
+  .text("Workout duration vs Workout date")
 };
 
